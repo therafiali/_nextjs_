9ab06cd4470f4fc5e6afcf257b34e2c410d825f2
Rafi Ali <therafiali@gmail.com>
2024-09-01 18:36:08 +0500

    first commit

---
+++ src/app/manage_students/page.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <DefaultLayout>
+    <div>
+        Hello
+    </div>
+  </DefaultLayout>
+  )
+}
+
+export default page
